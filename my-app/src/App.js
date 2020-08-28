2fdab3fa757dc05bc28368230c29d1409a9fc7d5
James-Hardy <jamesjhardy95@gmail.com>
2020-08-28 11:11:18 +0100

    add elm line for testing in isolation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import MyResponsiveLine from './line'
 import MyResponsiveLine2 from './nivoLine'
+import ElmLine from './elmLine'
+import styled from 'styled-components'
 
+const Container = styled.section`
+  width: 50vw;
+  height: 60vh;
+  background: white;
+`
 const data = [
   {
     id: 'Andrew Swihart',
@@ -34,11 +41,47 @@ const data = [
   },
 ]
 
+const data6 = [
+  {
+    id: 'Grocer Rate of Sale',
+    data: [
+      { x: '2020-03-26T00:00:00.000Z', y: 90 },
+      { x: '2020-03-27T00:00:00.000Z', y: 232.08 },
+      { x: '2020-03-28T00:00:00.000Z', y: 189 },
+      { x: '2020-03-29T00:00:00.000Z', y: 315.99 },
+      { x: '2020-03-30T00:00:00.000Z', y: 196.44 },
+      { x: '2020-03-31T00:00:00.000Z', y: 499.26 },
+      { x: '2020-04-01T00:00:00.000Z', y: 126 },
+      { x: '2020-04-02T00:00:00.000Z', y: 519.26 },
+      { x: '2020-04-03T00:00:00.000Z', y: 166.44 },
+      { x: '2020-04-04T00:00:00.000Z', y: 80.88 },
+      { x: '2020-04-05T00:00:00.000Z', y: 94.5 },
+      { x: '2020-04-06T00:00:00.000Z', y: 72 },
+      { x: '2020-04-07T00:00:00.000Z', y: 18 },
+      { x: '2020-04-08T00:00:00.000Z', y: 53.88 },
+      { x: '2020-04-09T00:00:00.000Z', y: 94.44 },
+      { x: '2020-04-10T00:00:00.000Z', y: 18 },
+      { x: '2020-04-12T00:00:00.000Z', y: 105.6 },
+      { x: '2020-04-13T00:00:00.000Z', y: 44.94 },
+      { x: '2020-04-14T00:00:00.000Z', y: 36 },
+      { x: '2020-04-15T00:00:00.000Z', y: 78 },
+      { x: '2020-04-16T00:00:00.000Z', y: 42 },
+      { x: '2020-04-17T00:00:00.000Z', y: 54 },
+      { x: '2020-04-18T00:00:00.000Z', y: 78 },
+      { x: '2020-04-19T00:00:00.000Z', y: 36 },
+      { x: '2020-04-20T00:00:00.000Z', y: 58.489999999999995 },
+      { x: '2020-04-21T00:00:00.000Z', y: 139.19 },
+    ],
+  },
+]
 function App() {
   return (
     <>
       <MyResponsiveLine data={data} />
       <MyResponsiveLine2 />
+      <Container>
+        <ElmLine data={data6} />
+      </Container>
     </>
   )
 }
---
+++ my-app/src/elmLine.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { ResponsiveLine } from '@nivo/line'
+
+export default function ElmLine({ data, legend, color }) {
+  return (
+    <ResponsiveLine
+      data={data}
+      margin={{ top: 50, right: 110, bottom: 80, left: 60 }}
+      xScale={{ type: 'point' }}
+      yScale={{
+        type: 'linear',
+        min: 'auto',
+        max: 'auto',
+        stacked: true,
+        reverse: false,
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        orient: 'bottom',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: -90,
+        legendOffset: 36,
+        legendPosition: 'middle',
+      }}
+      axisLeft={{
+        orient: 'left',
+        tickSize: 2,
+        tickPadding: 1,
+        tickRotation: 0,
+        legend,
+        legendOffset: -55,
+        legendPosition: 'middle',
+      }}
+      colors={color}
+      pointSize={7}
+      pointColor={color}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabel="y"
+      pointLabelYOffset={-12}
+      useMesh
+      animate
+      theme={{
+        axis: {
+          ticks: {
+            tickColor: '#eee',
+            text: {
+              textColor: '#eee',
+              fontSize: '12px',
+            },
+          },
+        },
+        fontFamily: 'futura',
+        tooltip: {
+          container: {
+            fontFamily: 'futura',
+            fontSize: '9px',
+            color: 'black',
+            background: 'white',
+          },
+        },
+      }}
+      legends={[
+        {
+          anchor: 'bottom-right',
+          direction: 'row',
+          justify: false,
+          translateX: 100,
+          rotateX: -90,
+          translateY: 0,
+          itemsSpacing: 0,
+          itemDirection: 'left-to-right',
+          itemWidth: 80,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: 'circle',
+          symbolBorderColor: 'rgba(0, 0, 0, .5)',
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemBackground: 'rgba(0, 0, 0, .03)',
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  )
+}
